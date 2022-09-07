5d672119da6b429b27897bf0616f89b915a0094b
Your Name <you@example.com>
2022-09-07 16:10:09 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import StepperBar from './Components/StepperBar';
 import MainForm from './Components/MainForm';
-import { useState } from 'react';
+
 
 function App() {
   return (
